827bac189241fb412ca4bde2f6852aded38e040e
NateHartley <nathanhartley82@gmail.com>
2025-04-13 19:00:37 +0100

    Implement main game principles

--- C_2.0/hl.c
+++ C_2.0/hl.c
@@ -24,66 +24,83 @@ int main() {
     srand(time(NULL));
     // Setting seed for rand
 
-    int* rand_num1 = malloc(sizeof(int));
-    int* rand_num2 = malloc(sizeof(int));
+    int *rand_num1 = malloc(sizeof(int));
+    int *rand_num2 = malloc(sizeof(int));
     // Malloc returns a pointer, so the type needs to be int* not int
     // Size of an int is 4 bytes
-
-    *rand_num1 = rand() % 10 + 1;
-    *rand_num2 = rand() % 10 + 1;
-    // Generate a random number between 0 and 10 and assign it to rand_num1 and rand_num2
-
-    printf("The first number is: %d \n", *rand_num1);
-    printf("Debug: %d \n", *rand_num2);
-
-    printf("Will the second number be higher [h] or lower [l] than the first? \n");
-    printf(">> ");
-
-    // TODO: implement comparison of usr_input and rand_num2
 
     char* usr_input = malloc(2);
     // Allocated 2 bytes, 1 byte for the user input (h or l), and one byte for the null character. If we only set malloc to be 1 then only the null char will be stored there
     // Reserved a memory size of 2 on the heap for this var, which means that piece of memory will always be available for this var, however I can still write into another memory address if this var is greater that the allocated memory size
     // Prevention of buffer overflow does not happen during malloc, it happens when you use the var in other functions e.g. fgets
 
-    if (fgets(usr_input, 2, stdin) != NULL){
-    // Takes in a user input from the standard input (terminal), last character appended is a null character '\0'
-    // E.g. size = 5, input = 'hello', output of that var = 'hell\0', output in terminal will look like = 'hell'
-    // Limited to a input size of 2 which is either 'h' or 'l' and the auto null character '\0'
+    while(1){
 
-        discard(usr_input);
-        // We can allocate a certain number of input characters to a variable in fgets, but if we write more than this then it won't affect the current variable, but it will affect other stuff
-        // These extra input characters are still in the standard input (terminal), they won't just disappear so this will bleed into other fgets unless we discard these overflow characters
+        // If score is greater than 1, include printf("The program is picking a new set of random numbers...")
+
+        *rand_num1 = rand() % 10 + 1;
+        *rand_num2 = rand() % 10 + 1;
+        // Generate a random number between 0 and 10 and assign it to the memory addresses of rand_num1 and rand_num2
+
+        printf("The first number is: %d \n", *rand_num1);
+        // Have to specify * before rand_num1 so that we are derefencing the pointer rand_num1 so we get the actual value stored at that memory address
+        printf("Debug rand_num2: %d \n", *rand_num2);
+
+        printf("\nWill the second number be higher [h] or lower [l] than the first? \n");
+        printf(">> ");
+
+        if (fgets(usr_input, 2, stdin) != NULL){
+        // Takes in a user input from the standard input (terminal), last character appended is a null character '\0'
+        // E.g. size = 5, input = 'hello', output of that var = 'hell\0', output in terminal will look like = 'hell'
+        // Limited to a input size of 2 which is either 'h' or 'l' and the auto null character '\0'
+        // fgets also overwrites what is already in usr_input
+
+            discard(usr_input);
+            // We can allocate a certain number of input characters to a variable in fgets, but if we write more than this then it won't affect the current variable, but it will affect other stuff
+            // These extra input characters are still in the standard input (terminal), they won't just disappear so this will bleed into other fgets unless we discard these overflow characters
+
+        }
+        printf("\n");
+        
+        if (strcmp(usr_input, "h") == 0 || strcmp(usr_input, "l") == 0) {
+            // strcmp compares strings, since every string is a char* we can just use usr_input and "h" with no *
+            // h and l have to be surrounded in double speak marks to make them literal strings, not literal chars
+
+            if (*rand_num1 < *rand_num2 && strcmp(usr_input, "h") == 0) {
+                // Need to dereference rand_num with * to get actual value
+
+                printf("Correct! The second number, %d, is higher than the first, %d \n", *rand_num2, *rand_num1);
+
+            } else if (*rand_num1 > *rand_num2 && strcmp(usr_input, "h") == 0){
+                printf("Incorrect... The second number, %d, is lower than the first, %d \n", *rand_num2, *rand_num1);
+                break;
+
+            } else if (*rand_num1 > *rand_num2 && strcmp(usr_input, "l") == 0){
+                printf("Correct! The second number, %d, is lower than the first, %d \n", *rand_num2, *rand_num1);
+
+            } else if (*rand_num1 < *rand_num2 && strcmp(usr_input, "l") == 0){
+                printf("Incorrect... The second number, %d, is higher than the first, %d \n", *rand_num2, *rand_num1);
+                break;
+
+            } else {
+                printf("They are the same!\n");
+            }
+        } else {
+            printf("This is not a valid input\n");
+            break;
+        }
+        printf("\n");
 
     }
+
+    printf("\nGAME OVER - You finished the game with X points ");
     
-    printf("print usr_input >  %s \n", usr_input);
     
     free(usr_input);
     free(rand_num1);
     free(rand_num2);
     // Free the variables once there is no more use for it to release its allocated memory and prevent a memory leak
 
-
-
-
-    int TEST_SIZE = 5;
-    char test_string[TEST_SIZE]; // allocated on stack
-    
-    fgets(test_string, TEST_SIZE, stdin);
-
-    printf("test>> %s \n", test_string);
-
-
+    return 0;
+    // main function is an int so need to return 0 afterwards to specify that the function ran without error
 }
-
-
-/*
-
-- Choose random number 1
-- Choose random number 2
-- Ask user if 1 is higher or lower than 2
-- If correct, award points, pick another two random numbers, loop
-- If wrong, print total points, end
-
-*/
